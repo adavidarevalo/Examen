a4aec35783c8587e9ced641a6019344766f0ba2b
adavidarevalo <davidarevaloc20@gmail.com>
2023-12-14 18:21:20 -0500

    Se agrego la db

--- Docs/Base_Datos.sql
+++ Docs/Base_Datos.sql
@@ -1,156 +1,224 @@
-
-
+-- -------------------------------------------------------------
+-- TablePlus 5.6.6(520)
 --
--- Base de datos: `Sexto`
+-- https://tableplus.com/
 --
+-- Database: Sexto
+-- Generation Time: 2023-12-14 18:19:15.9040
+-- -------------------------------------------------------------
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `Pais`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE `Pais` (
-  `PaisId` int(11) NOT NULL,
-  `Nombre` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Volcado de datos para la tabla `Pais`
---
-
-INSERT INTO `Pais` (`PaisId`, `Nombre`) VALUES
-(1, 'ECUADOR'),
-(8, 'Colombia'),
-(9, 'PERU');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `Productos`
---
-
-CREATE TABLE `Productos` (
-  `ProductoId` int(11) NOT NULL,
-  `Nombre` text NOT NULL,
-  `Precio_Compra` decimal(8,2) NOT NULL,
-  `Precio_Venta` decimal(8,2) NOT NULL,
-  `Iva` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL,
-  `Unidad_Medida` text NOT NULL,
-  `Imagen` text NOT NULL,
-  `Fecha` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `Provincias`
---
-
-CREATE TABLE `Provincias` (
-  `ProvinciasId` int(11) NOT NULL,
-  `Nombre` text NOT NULL,
-  `PaisesId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `Provincias`
---
-
-INSERT INTO `Provincias` (`ProvinciasId`, `Nombre`, `PaisesId`) VALUES
-(1, 'Tungurahua', 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `Usuarios`
---
-
+DROP TABLE IF EXISTS `Usuarios`;
 CREATE TABLE `Usuarios` (
-  `UsuarioId` int(11) NOT NULL,
-  `Cedula` varchar(17) NOT NULL,
-  `Nombres` varchar(100) NOT NULL,
-  `Apellidos` varchar(100) NOT NULL,
-  `Telefono` varchar(17) NOT NULL,
-  `Correo` varchar(150) NOT NULL,
-  `Contrasenia` text NOT NULL,
-  `Rol` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `Usuarios`
---
+  `UsuarioId` int(11) NOT NULL AUTO_INCREMENT,
+  `Cedula` varchar(17) DEFAULT NULL,
+  `Nombres` varchar(100) DEFAULT NULL,
+  `Apellidos` varchar(100) DEFAULT NULL,
+  `Telefono` varchar(17) DEFAULT NULL,
+  `Correo` varchar(150) DEFAULT NULL,
+  `Contrasenia` text,
+  `Rol` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`UsuarioId`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;;
 
 INSERT INTO `Usuarios` (`UsuarioId`, `Cedula`, `Nombres`, `Apellidos`, `Telefono`, `Correo`, `Contrasenia`, `Rol`) VALUES
-(1, '1803971371', 'Luis Antonio', 'Llerena Ocaña', '0987654321', 'lleroc1@gmail.com', '202cb962ac59075b964b07152d234b70', 'Administrador'),
+(1, '123456789', 'Juan', 'Pérez', '555-1234', 'juan@example.com', 'hashed_password_1', 'Admin'),
+(2, '987654321', 'María', 'Gómez', '555-5678', 'maria@example.com', 'hashed_password_2', 'Usuario'),
+(3, '111223344', 'Pedro', 'Rodríguez', '555-8765', 'pedro@example.com', 'hashed_password_3', 'Moderador'),
+(4, '123456789', 'Juan', 'Pérez', '555-1234', 'juan@example.com', 'hashed_password_1', 'Admin'),
+(5, '987654321', 'María', 'Gómez', '555-5678', 'maria@example.com', 'hashed_password_2', 'Usuario'),
+(6, '111223344', 'Pedro', 'Rodríguez', '555-8765', 'pedro@example.com', 'hashed_password_3', 'Moderador');
 
-COMMIT;
 
---
--- Índices para tablas volcadas
---
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- -------------------------------------------------------------
+-- TablePlus 5.6.6(520)
 --
--- Indices de la tabla `Pais`
+-- https://tableplus.com/
 --
-ALTER TABLE `Pais`
-  ADD PRIMARY KEY (`PaisId`);
+-- Database: Sexto
+-- Generation Time: 2023-12-14 18:19:46.9370
+-- -------------------------------------------------------------
 
---
--- Indices de la tabla `Productos`
---
-ALTER TABLE `Productos`
-  ADD PRIMARY KEY (`ProductoId`);
 
---
--- Indices de la tabla `Provincias`
---
-ALTER TABLE `Provincias`
-  ADD PRIMARY KEY (`ProvinciasId`),
-  ADD KEY `Pais_Provincia` (`PaisesId`);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Indices de la tabla `Usuarios`
---
-ALTER TABLE `Usuarios`
-  ADD PRIMARY KEY (`UsuarioId`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
+DROP TABLE IF EXISTS `Películas`;
+CREATE TABLE `Películas` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_pelicula` int(11) DEFAULT NULL,
+  `ID_cine` int(11) DEFAULT NULL,
+  `Título` varchar(255) NOT NULL,
+  `Género` varchar(100) DEFAULT NULL,
+  `Duración` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `ID_cine` (`ID_cine`),
+  CONSTRAINT `películas_ibfk_1` FOREIGN KEY (`ID_cine`) REFERENCES `Cines` (`ID_cine`)
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4;;
 
---
--- AUTO_INCREMENT de la tabla `Pais`
---
-ALTER TABLE `Pais`
-  MODIFY `PaisId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+INSERT INTO `Películas` (`ID`, `ID_pelicula`, `ID_cine`, `Título`, `Género`, `Duración`) VALUES
+(53, 70652, 6, 'Bambi', 'animada', 157);
 
---
--- AUTO_INCREMENT de la tabla `Productos`
---
-ALTER TABLE `Productos`
-  MODIFY `ProductoId` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `Provincias`
---
-ALTER TABLE `Provincias`
-  MODIFY `ProvinciasId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- -------------------------------------------------------------
+-- TablePlus 5.6.6(520)
 --
--- AUTO_INCREMENT de la tabla `Usuarios`
+-- https://tableplus.com/
 --
-ALTER TABLE `Usuarios`
-  MODIFY `UsuarioId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+-- Database: Sexto
+-- Generation Time: 2023-12-14 18:20:09.1380
+-- -------------------------------------------------------------
 
---
--- Restricciones para tablas volcadas
---
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+DROP TABLE IF EXISTS `Generos`;
+CREATE TABLE `Generos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_genero` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre_genero` (`nombre_genero`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4;;
+
+INSERT INTO `Generos` (`id`, `nombre_genero`) VALUES
+(14, 'comedia'),
+(16, 'accion'),
+(17, 'animada');
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- -------------------------------------------------------------
+-- TablePlus 5.6.6(520)
 --
--- Filtros para la tabla `Provincias`
+-- https://tableplus.com/
 --
-ALTER TABLE `Provincias`
-  ADD CONSTRAINT `Pais_Provincia` FOREIGN KEY (`PaisesId`) REFERENCES `Pais` (`PaisId`);
-COMMIT;
+-- Database: Sexto
+-- Generation Time: 2023-12-14 18:20:40.0090
+-- -------------------------------------------------------------
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+DROP TABLE IF EXISTS `Ciudades`;
+CREATE TABLE `Ciudades` (
+  `ID_ciudad` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_ciudad` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID_ciudad`),
+  UNIQUE KEY `nombre_ciudad` (`nombre_ciudad`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;;
+
+INSERT INTO `Ciudades` (`ID_ciudad`, `nombre_ciudad`) VALUES
+(10, 'loja'),
+(12, 'ambato'),
+(13, 'quito'),
+(14, 'imbabura');
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- -------------------------------------------------------------
+-- TablePlus 5.6.6(520)
+--
+-- https://tableplus.com/
+--
+-- Database: Sexto
+-- Generation Time: 2023-12-14 18:20:58.6430
+-- -------------------------------------------------------------
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+DROP TABLE IF EXISTS `Cines`;
+CREATE TABLE `Cines` (
+  `ID_cine` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(255) DEFAULT NULL,
+  `Ciudad` varchar(255) DEFAULT NULL,
+  `Número_salas` int(11) DEFAULT NULL,
+  `Direccion` varchar(255) DEFAULT NULL,
+  `Teléfono` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_cine`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;;
+
+INSERT INTO `Cines` (`ID_cine`, `Nombre`, `Ciudad`, `Número_salas`, `Direccion`, `Teléfono`) VALUES
+(5, 'Cinemark', 'ambato', 24, 'Mall de los Andes', 995402222),
+(6, 'Cineopolis', 'quito', 12, '12 De Noviembre', 995402222),
+(7, 'VHS', 'imbabura', 34, '12 De Noviembre', 995402222);
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
